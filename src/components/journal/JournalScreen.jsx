54ca299877ca4aec153659c6de1a5d69d7201e86
joselrdg <josesietepicos@gmail.com>
2021-04-11 02:30:24 +0200

    style

---
+++ src/components/journal/JournalScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const JournalScreen = () => {
+    return (
+        <div>
+            <h1>Hola desde jouuur</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { JournalApp } from './JournalApp';
 import reportWebVitals from './reportWebVitals';
+import './styles/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <JournalApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
