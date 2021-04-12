ed246b6504d465f8322956da6a1be65179fec8bd
joselrdg <josesietepicos@gmail.com>
2021-04-12 23:28:19 +0200

    scss

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React from "react";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
-    return (
-        <div className='notes__main-content'>
-            <NotesAppBar/>
+  return (
+    <div className="notes__main-content">
+      <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+        />
+        <textarea
+          placeholder="What happened today"
+          className="notes__textarea"
+        ></textarea>
+        <div className="note__image">
+          <img
+            src="https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png"
+            alt="image"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
